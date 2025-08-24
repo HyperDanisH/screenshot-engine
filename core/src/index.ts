d8c129776d2036117d2f12f26fa03dc16bc52583
HyperDanisH <hypertxt.assin@gmail.com>
2025-08-24 07:16:53 +0000

    Init Browser logic

--- core/src/index.ts
+++ core/src/index.ts
@@ -1,9 +1,39 @@
 import express, { Request, Response } from "express"
+import bodyParser from 'body-parser';
+import { chromium, Browser, BrowserContext, Route, Request as PlaywrightRequest, Page } from 'playwright';
+import dotenv from 'dotenv';
+import UserAgent from 'user-agents';
+
+dotenv.config()
+
+const port = 3000
 
 const app = express()
+
+let browser: Browser;
+
+const initializeBrowser = async () => {
+  browser = await chromium.launch({
+    headless: true,
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage',
+      '--disable-accelerated-2d-canvas',
+      '--no-first-run',
+      '--no-zygote',
+      '--single-process',
+      '--disable-gpu'
+    ]
+  });
+};
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World')
 })
 
-app.listen(3000)
+app.listen(port, () => {
+  initializeBrowser().then(() => {
+    console.log(`Server is running on port ${port}`);
+  });
+});
